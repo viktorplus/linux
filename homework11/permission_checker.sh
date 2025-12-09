229caf4316297f72ba581436f746d70beb12d322
viktorplus <viktorplus3@gmail.com>
2025-12-09 11:57:48 +0100

    Update

--- homework11/permission_checker.sh
+++ homework11/permission_checker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /opt/101025-ptm/ 
-ls -la *.sh .*.sh 2>/dev/null | awk '{print $1, $9}'
+ls -la *.sh | awk '{print $1, $9}'
 find . -maxdepth 1 -name '*.sh' -type f -exec chmod +x {} +
-find . -maxdepth 1 -name '.*.sh' -type f -exec chmod +x {} +
